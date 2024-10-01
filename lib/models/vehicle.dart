17f2246a0ad8cbf3d615f8ba4d7d2c4f722fbebd
Anna Nilsson-Niia <annanilssonniia@gmail.com>
2024-10-01 22:37:12 +0200

    Change name on variable in vehicle.dart

--- lib/models/vehicle.dart
+++ lib/models/vehicle.dart
@@ -2,9 +2,9 @@ import 'package:parking_one/enums/vehicle_type.dart';
 import 'package:parking_one/models/person.dart';
 
 class Vehicle {
-  String? numberPlate;
+  String? licensePlate;
   Person owner;
   VehicleType vehicleType;
 
-  Vehicle(this.numberPlate, this.owner, this.vehicleType);
+  Vehicle(this.licensePlate, this.owner, this.vehicleType);
 }
